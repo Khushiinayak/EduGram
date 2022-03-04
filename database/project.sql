820638f79196dbb4b64dfde806bc71965f7a5bb7
Khushi <98007258+Khushiinayak@users.noreply.github.com>
2022-03-04 08:41:13 +0530

    Add files via upload

---
+++ database/project.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 31, 2022 at 11:56 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+-- Error reading structure for table project.category: #1932 - Table 'project.category' doesn't exist in engine
+-- Error reading data for table project.category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `project`.`category`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quacat`
+--
+-- Error reading structure for table project.quacat: #1932 - Table 'project.quacat' doesn't exist in engine
+-- Error reading data for table project.quacat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `project`.`quacat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quans`
+--
+-- Error reading structure for table project.quans: #1932 - Table 'project.quans' doesn't exist in engine
+-- Error reading data for table project.quans: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `project`.`quans`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table project.users: #1932 - Table 'project.users' doesn't exist in engine
+-- Error reading data for table project.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `project`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
